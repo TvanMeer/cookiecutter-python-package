5517c7308129128ee2465d3cf6dfa96ac4d58d7c
TvanMeer <tvanmeer123@gmail.com>
2021-11-30 05:07:26 +0100

    Add poetry actions

--- hooks/post_gen_project.sh
+++ hooks/post_gen_project.sh
@@ -1,3 +1,14 @@
 #!/bin/bash
 
+
+# Install dependencies
+poetry install
+
+# Create lock file
+poetry lock
+
+# Build tarball and wheel in dist folder
+poetry build
+
+
 echo "Project created succesfully!"
