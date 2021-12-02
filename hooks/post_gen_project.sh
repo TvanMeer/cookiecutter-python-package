657c1eb2b682c80525b0add90a8fdbe8bd0c8adf
TvanMeer <tvanmeer123@gmail.com>
2021-12-02 05:24:14 +0100

    Not build wheel by default

--- hooks/post_gen_project.sh
+++ hooks/post_gen_project.sh
@@ -4,14 +4,14 @@
 echo "Installing dependencies with poetry..."
 
 
-# Install dependencies
+# Install dependencies and package in .venv
 poetry install
 
 # Create lock file
 poetry lock
 
 # Build tarball and wheel in dist folder
-poetry build
+# poetry build
 
 
 echo "Project created succesfully!"
