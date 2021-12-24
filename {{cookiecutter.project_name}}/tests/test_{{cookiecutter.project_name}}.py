cdf6f55117e5ded167d39ae4a6cb7b8f085fa631
TvanMeer <tvanmeer123@gmail.com>
2021-12-24 03:25:44 +0100

    Make version number a cookiecutter variable

--- {{cookiecutter.project_name}}/tests/test_{{cookiecutter.project_name}}.py
+++ {{cookiecutter.project_name}}/tests/test_{{cookiecutter.project_name}}.py
@@ -2,4 +2,4 @@ from {{cookiecutter.project_name}} import __version__
 
 
 def test_version():
-    assert __version__ == '0.1.0'
+    assert __version__ == "{{cookiecutter.version}}"
--- {{cookiecutter.project_name}}/{{cookiecutter.project_name}}/__init__.py
+++ {{cookiecutter.project_name}}/{{cookiecutter.project_name}}/__init__.py
@@ -1,3 +1,3 @@
-__version__ = '0.1.0'
+__version__ = "{{cookiecutter.version}}"
 
 from {{cookiecutter.project_name}}.client import Client # noqa
