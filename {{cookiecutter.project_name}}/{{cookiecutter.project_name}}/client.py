2870aa2aa6aad449e53402db74b941a6a660de9e
TvanMeer <tvanmeer123@gmail.com>
2021-12-25 06:37:16 +0100

    Add sample inline documentation

--- {{cookiecutter.project_name}}/{{cookiecutter.project_name}}/client.py
+++ {{cookiecutter.project_name}}/{{cookiecutter.project_name}}/client.py
@@ -3,6 +3,28 @@ from .module2.entrypoint2 import Y
 
 
 class Client:
+    """Example of an inline documented class.
+
+    This class contains client exposed functions that are part of the API.
+    These functions use uncoupled implementation code in the ``module1`` and
+    ``module2`` modules.
+
+    Properties created with the ``@property`` decorator should be documented
+    in the property's getter method. This docstring is used for both the getter
+    and setter.
+
+    Args:
+        arg1 (int): Some argument passed to the `__init__` function.
+
+    Attributes:
+        attr1 (int): Some attribute
+        attr2 (str): Another attribute
+    """
+
+    def __init__(self, arg1: int) -> None:
+        self.attr1 = arg1
+        self.attr2 = "foo"
+
     def func_1(self, arg1: int, arg2: list[str]) -> bool:
         """Demo function 1
 
@@ -10,36 +32,65 @@ class Client:
         renders this inline documentation. Google style inline docs are used.
 
         Args:
-            arg1: Argument 1
-            arg2: Argument 2
+            arg1 (int): Argument 1
+            arg2 (list[str]): Argument 2
 
         Returns:
-            Some return value
+            bool: Some return value
         """
         x = X()
         succes = x.do_something()
         return succes
 
-    def func_2(self, arg1: int = None, arg2: str = "foo") -> bool:
+    def func_2(self, arg1: int, arg2: str = "foo") -> bool:
         """Demo function 2
 
         This is a demo of all possible inline documentation components
         for a function.
 
-        Note:
-            Do not include the `self` parameter in the ``Args`` section.
-
         Args:
-            arg1: Description of arg1. Defaults to None.
-            arg2: Description of arg2. Defaults to "foo".
+            arg1 (int): Description of arg1.
+
+        Keyword Args:
+            arg2 (str): Description of arg2. Defaults to "foo".
 
         Returns:
-            Some return value
+            bool: Some return value
 
         Raises:
             AttributeError: The ``Raises`` section is a list of all exceptions
                 that are relevant to the interface.
             ValueError: If `param2` is equal to `param1`.
+
+        Note:
+            Do not include the `self` parameter in the ``Args`` section.
+
+        Attention:
+            Some attention message.
+
+        Hint:
+            Some hint message.
+
+        Tip:
+            Some tip message.
+
+        Important:
+            Some important message.
+
+        Caution:
+            Some caution message.
+
+        Danger:
+            Some danger message.
+
+        Error:
+            Some error message.
+
+        References:
+            Some reference.
+
+        See Also:
+            func1: Is related to this function
 
         Examples:
             Examples should be written in doctest format, and should illustrate how
